8c1f37668133d3a7d3b9967f685cadca473b588b
Andy Leitermann <andyleitermann@gmail.com>
2014-05-21 15:16:44 -0600

    skeletal version

--- shellargs.py
+++ shellargs.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python 
 
-""" checknet: checks various network settings on a Cache-A to attempt to
-        determine the source of network issues.  """
+"""     shellargs.py
+        """
+
+import sys
 
 def sh(script, stdin=None):
     """Returns (stdout, stderr)"""
@@ -15,50 +17,10 @@ def sh(script, stdin=None):
     stdout, stderr = proc.communicate()
     return stdout, stderr
 
-# welcome message 
-print "CheckNet network setus analysis tool"
+##############################################################################
 
-# generate empty output file 
-print "generating diagnostic file..."
-output_file = "/media/vtape/network-analysis.txt"
-sh('touch ' + output_file + ' | chmod 777 ' + output_file)
+print sys.argv
 
-# preload useful information into output_file
-sh('echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! IFCONFIG:" >> ' + output_file)
-sh('ifconfig -a >> ' + output_file)
-sh('echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! ROUTE:" >> ' + output_file)
-sh('route >> ' + output_file)
-sh('echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! IFCFG FILES:" >> ' + output_file)
-sh('cat /etc/sysconfig/network-scripts/ifcfg-eth0 >> ' + output_file)
-sh('cat /etc/sysconfig/network-scripts/ifcfg-eth1 >> ' + output_file)
-sh('cat /etc/sysconfig/network-scripts/ifcfg-lo >> ' + output_file)
-sh('cat /etc/sysconfig/network-scripts/ifcfg-eth2 >> ' + output_file)
-sh('cat /etc/sysconfig/network-scripts/ifcfg-eth3 >> ' + output_file)
-sh('echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! NETSTAT:" >> ' + output_file)
-sh('netstat -alp >> ' + output_file)
-sh('echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! MESSAGES:" >> ' + output_file)
-sh('tail -n 100 /var/log/messages >> ' + output_file) # prints the last 100 lines of /var/log/messages
-
-# ping loopback to verify IP functionality
-print "\n" + "%"*53 + "\nChecking loopback interface..."
-loopback = sh('ping -q -c10 127.0.0.1 | tee -a ' + output_file)
-loopback_percent = loopback[0].split(',')[-2]
-if loopback_percent == " 0% packet loss":
-    print "\tloopback _GOOD_, Internet Protocol is functional"
-elif loopback_percent == " 100% packet loss":
-    print "\tloopback _DOWN_, Internet Protocol is not functional"
-else:
-    print "\tloopback ping returned " + loopback_percent[1:4] + " _ERRORS_"
-
-# ping default gateway listed in route
-print "Checking default gateway..."
-gateway = sh('route')[0].split()[-7]
-print gateway
-
-# ping support.cache-a.com's IP to verify internet, double check with cache-a.com 
-
-# ping support.cache-a.com and cache-a.com 
-
-# check and print info from ifcfg-... files 
-
-
+def argparser(arg_list):
+    #add stuff here
+    return 
